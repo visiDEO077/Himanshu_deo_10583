f00a764d596928f8e194536126d5d5dd51392bc7
visiDEO077 <167595642+visiDEO077@users.noreply.github.com>
2026-01-08 12:42:45 +0530

    Update Marks grading and total calculation system.cpp

--- Marks grading and total calculation system.cpp
+++ Marks grading and total calculation system.cpp
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main(){
+  int num_sub;
+
+printf("Enter no. of subjects");
+scanf("%d",&num_sub);
+int sub_marks[num_sub];
+for(int i=0;i<num_sub;i++){
+  scanf("%d",&sub_marks[i]);
+  
+}
+int sub=0;
+for(int j=0;j<num_sub;j++){
+sum+=sub_marks[j];
+}
+float percentage;
+percentage=(sum/(num_sub*100))*100;
+printf("Total Marks : %d\n",sum);
+printf("Total percentage : %d\n",percentage);
 
